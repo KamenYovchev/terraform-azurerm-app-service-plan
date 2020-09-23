27f71ca47c0b0957bf03946174a1f9f00219d026
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-09-23 19:12:20 +0300

    add resources for module

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "app_service_paln_id" {
+    value = azurerm_app_service_plan.app_service_plan.id
+}
+
+output "app_sercice_plan_name" {
+    value = azurerm_app_service_plan.app_service_plan.name
+}
